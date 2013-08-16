5850c77140c35f21893e3de856e6baf4c627e9aa
wangqifeng <qifeng_wang@yeah.net>
2013-08-16 17:29:56 +0800

    fix bugs

---
+++ src/liblbfgs/LBFGSClient.java
@@ -0,0 +1,16 @@
+
+package liblbfgs;
+
+/** 
+ * LBFGSClient 接口，调用LBFGS需实现此接口
+ *
+ * <p>detailed comment
+ * @author wangqifeng
+ * @date 2013 8 15 10:33:46 
+ * @since  
+ */
+
+public interface LBFGSClient extends lbfgs_evaluate_t, lbfgs_progress_t
+{
+
+}
--- src/liblbfgs/lbfgs_parameter_t.java
+++ src/liblbfgs/lbfgs_parameter_t.java
@@ -18,7 +18,7 @@ public class lbfgs_parameter_t
      *  (corrections). The default value is \c 6. Values less than \c 3 are
      *  not recommended. Large values will result in excessive computing time.
      */
-    int m;
+    public int m;
 
     /**
      * Epsilon for convergence test.
@@ -28,7 +28,7 @@ public class lbfgs_parameter_t
      *  where ||.|| denotes the Euclidean (L2) norm. The default value is
      *  \c 1e-5.
      */
-    double epsilon;
+    public double epsilon;
 
     /**
      * Distance for delta-based convergence test.
@@ -37,7 +37,7 @@ public class lbfgs_parameter_t
      *  parameter is zero, the library does not perform the delta-based
      *  convergence test. The default value is \c 0.
      */
-    int past;
+    public int past;
 
     /**
      * Delta for convergence test.
@@ -49,7 +49,7 @@ public class lbfgs_parameter_t
      *  the objective value of the current iteration.
      *  The default value is \c 0.
      */
-    double delta;
+    public double delta;
 
     /**
      * The maximum number of iterations.
@@ -59,21 +59,21 @@ public class lbfgs_parameter_t
      *  optimization process until a convergence or error. The default value
      *  is \c 0.
      */
-    int max_iterations;
+    public int max_iterations;
 
     /**
      * The line search algorithm.
      *  This parameter specifies a line search algorithm to be used by the
      *  L-BFGS routine.
      */
-    int linesearch;
+    public int linesearch;
 
     /**
      * The maximum number of trials for the line search.
      *  This parameter controls the number of function and gradients evaluations
      *  per iteration for the line search routine. The default value is \c 20.
      */
-    int max_linesearch;
+    public int max_linesearch;
 
     /**
      * The minimum step of the line search routine.
@@ -82,7 +82,7 @@ public class lbfgs_parameter_t
      *  problem is extremely badly scaled (in which case the exponents should
      *  be increased).
      */
-    double min_step;
+    public double min_step;
 
     /**
      * The maximum step of the line search.
@@ -91,14 +91,14 @@ public class lbfgs_parameter_t
      *  problem is extremely badly scaled (in which case the exponents should
      *  be increased).
      */
-    double max_step;
+    public double max_step;
 
     /**
      * A parameter to control the accuracy of the line search routine.
      *  The default value is \c 1e-4. This parameter should be greater
      *  than zero and smaller than \c 0.5.
      */
-    double ftol;
+    public double ftol;
 
     /**
      * A coefficient for the Wolfe condition.
@@ -109,7 +109,7 @@ public class lbfgs_parameter_t
      *  The default value is \c 0.9. This parameter should be greater
      *  the \ref ftol parameter and smaller than \c 1.0.
      */
-    double wolfe;
+    public double wolfe;
 
     /**
      * A parameter to control the accuracy of the line search routine.
@@ -121,7 +121,7 @@ public class lbfgs_parameter_t
      *  greater than the \ref ftol parameter (\c 1e-4) and smaller than
      *  \c 1.0.
      */
-    double gtol;
+    public double gtol;
 
     /**
      * The machine precision for floating-point values.
@@ -130,7 +130,7 @@ public class lbfgs_parameter_t
      *  with the status code (::LBFGSERR_ROUNDING_ERROR) if the relative width
      *  of the interval of uncertainty is less than this parameter.
      */
-    double xtol;
+    public double xtol;
 
     /**
      * Coeefficient for the L1 norm of variables.
@@ -145,7 +145,7 @@ public class lbfgs_parameter_t
      *  the function value F(x) and gradients G(x) as usual. The default value
      *  is zero.
      */
-    double orthantwise_c;
+    public double orthantwise_c;
 
     /**
      * Start index for computing L1 norm of the variables.
@@ -159,7 +159,7 @@ public class lbfgs_parameter_t
      *  variables, x_1, ..., x_{b-1} (e.g., a bias term of logistic
      *  regression) from being regularized. The default value is zero.
      */
-    int orthantwise_start;
+    public int orthantwise_start;
 
     /**
      * End index for computing L1 norm of the variables.
@@ -168,7 +168,7 @@ public class lbfgs_parameter_t
      *  specifies the index number at which the library stops computing the
      *  L1 norm of the variables x,
      */
-    int orthantwise_end;
+    public int orthantwise_end;
     
     public lbfgs_parameter_t()
     {
